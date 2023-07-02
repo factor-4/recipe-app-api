4dc1362a1ba5fc32e230c15c4a3e6e83f3d137d1
unknown <snk.sojib@gmail.com>
2023-07-03 01:40:04 +0600

    phycopg2 added

---
+++ app/app/tests.py
@@ -0,0 +1,16 @@
+"""
+sample test
+"""
+
+from django.test import SimpleTestCase
+
+from app import calc
+
+class CalcTests(SimpleTestCase):
+
+
+    def test_add_numbers(self):
+
+        res = calc.add(5,6)
+
+        self.assertEqual(res, 11)
